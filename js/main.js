5e10c90079ce776ddc764ecfcccc8cab58fa0a9b
dimamuha637296 <dima.muha637296@gmail.com>
2021-10-07 03:48:59 +0300

    add header padding script

--- js/main.js
+++ js/main.js
@@ -505,4 +505,64 @@ $(document).ready(function(){
 		});
 	}();
 
+	!function () {
+		'use strict';
+
+		var oWindow = $(window);
+
+		function setMainPadding() {
+			var header = $('.header');
+			var main = $('.outer-bg');
+			if (!header.length) {
+				return false;
+			}
+			var position = header.css('position');
+			var headerHeight = header.outerHeight();
+			if (position === 'fixed') {
+				main.css({
+					'padding-top': headerHeight
+				});
+			} else {
+				main.css({
+					'padding-top': ''
+				});
+			}
+		}
+
+		var scrollHeader = {
+			init: function () {
+				var header = $('.header__desktop');
+				if (!header.length) {
+					return false;
+				}
+
+				$(window).off('scroll.scrollHeader').on('scroll.scrollHeader', function() {
+					var headerHeight = header.outerHeight();
+					var headerPosition = header.offset().top;
+					if (headerPosition > headerHeight) {
+						header.addClass('header__desktop--scroll');
+						setMainPadding();
+					} else {
+						header.removeClass('header__desktop--scroll');
+						setMainPadding();
+					}
+				});
+			}
+		};
+
+		$(function () {
+			setMainPadding();
+			scrollHeader.init();
+		});
+
+		oWindow.on('load', function () {
+			setMainPadding();
+		});
+
+		oWindow.on('resize', function () {
+			setMainPadding();
+		});
+	}();
+
+
 })
